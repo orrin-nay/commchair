f77a6eb00a48bf03e01a05a4e166824cdc45c7dd
Cameron J. Yeomans <cameron.yeomans@uvm.edu>
2018-09-08 16:11:51 -0600

    changed icon

---
+++ src/app/component-gallery/component-gallery.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-component-gallery',
+  templateUrl: './component-gallery.component.html',
+  styleUrls: ['./component-gallery.component.css']
+})
+export class ComponentGalleryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
