50969188fccaf4c2997e7fc34a9cd5a45e6eaa65
Cameron J. Yeomans <cameron.yeomans@uvm.edu>
2018-09-08 17:27:54 -0600

    added mobile site

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'commchair';
+	
+	function toggle() {
+				var element = document.getElementById("navCollapse");
+				element.classList.toggle("visable");
+			}
+		
+  
 }
