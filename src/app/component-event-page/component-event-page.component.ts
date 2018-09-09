f61e8af92f43d2b750345a24d06d03a2397b1d30
Cameron J. Yeomans <cameron.yeomans@uvm.edu>
2018-09-09 10:20:48 -0600

    risky commit

--- src/app/component-event-page/component-event-page.component.ts
+++ src/app/component-event-page/component-event-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { EventsService } from '../services/events/events.service';
 import{ HomeComponentComponent } from '../home-component/home-component.component'
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-component-event-page',
@@ -9,9 +10,11 @@ import{ HomeComponentComponent } from '../home-component/home-component.componen
 })
 export class ComponentEventPageComponent implements OnInit {
 
-  constructor(private homecomponentcomponent: HomeComponentComponent) { }
+	private eventId : string; 
+  constructor(private route : ActivatedRoute) { }
 
   ngOnInit() {
+	  this.eventId = this.route.snapshot.paramMap.get("eventid");
   }
   
 
