5b644bf3cd9a1a71610155fdadf767328650fcf7
Cameron J. Yeomans <cameron.yeomans@uvm.edu>
2018-09-09 03:38:12 -0600

    mobile site main page

--- src/app/home-component/home-component.component.ts
+++ src/app/home-component/home-component.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { EventsService } from '../services/events/events.service';
+import {AppComponent} from '../app.component';
 
 @Component({
   selector: 'app-home-component',
@@ -7,12 +8,14 @@ import { EventsService } from '../services/events/events.service';
   styleUrls: ['./home-component.component.css']
 })
 export class HomeComponentComponent implements OnInit {
-  constructor(private eventsService: EventsService) {
+  constructor(private eventsService: EventsService, private appComponent : AppComponent) {
     eventsService.getEvents().subscribe(events => {
       this.events = events;
     });
   }
   events: any = [];
 
+  notMobile = this.appComponent.mobileNavVisible;
+  
   ngOnInit() {}
 }
