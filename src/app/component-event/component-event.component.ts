f14dc8e874ca3919e19a13f755ba0302cf7c120a
Orrin <naylor.orrin@gmail.com>
2018-09-09 09:20:56 -0600

    fixed some of the profile issues

--- src/app/component-event/component-event.component.ts
+++ src/app/component-event/component-event.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, FormGroupDirective, NgForm, Validators, FormBuilder } from '@angular/forms';
 import { EventsService } from '../services/events/events.service';
+import { SkillsService, Skill } from '../services/skills/skills.service';
 @Component({
   selector: 'app-component-event',
   templateUrl: './component-event.component.html',
@@ -9,12 +10,16 @@ import { EventsService } from '../services/events/events.service';
 export class ComponentEventComponent implements OnInit {
 
   form: FormGroup;
+  skills: Skill[];
+  addedSkills: Skill[];
 
 
   constructor(
     private formBuilder: FormBuilder,
     private eventService: EventsService,
+    private skillsService: SkillsService
   ) { 
+    this.addedSkills = [];
     this.form = this.formBuilder.group({
       name: ['', [
         Validators.required,
@@ -24,8 +29,12 @@ export class ComponentEventComponent implements OnInit {
       ]],
       organization: ['', [
         Validators.required,
-      ]]
-    })
+      ]],
+      skillName: [],
+    });
+    skillsService.getSkills().subscribe(skills => {
+      this.skills = skills;
+    });
   }
 
   ngOnInit() {
@@ -38,5 +47,54 @@ export class ComponentEventComponent implements OnInit {
     this.eventService.createEvent(name, description, organization)
       .subscribe(e => console.log(e));
   }
-
+  addSkill() {
+    if (!this.addedSkills) {
+      this.addedSkills = [];
+    }
+    const doesExist = this.addedSkills.findIndex(
+      o1 => o1.name === this.form.value.skillName
+    );
+    if (!doesExist) {
+      this.skills.forEach(skillFromDb => {
+        if (skillFromDb.name === this.form.value.skillName) {
+          this.addedSkills.push(skillFromDb);
+        }
+      });
+    }
+    this.form.value.skillName = '';
+    console.log(this.addedSkills)
+  }
+  canSubmit() {
+    const doesExist = this.addedSkills.findIndex(
+      o1 => o1.name === this.form.value.skillName
+    );
+    if (!doesExist) {
+      return false;
+    }
+    let skillExists = false;
+    this.skills.forEach(skill => {
+      if (skill.name === this.form.value.skillName) {
+        skillExists = true;
+      }
+    });
+    return skillExists;
+  }
+  skillsThatTheEventDoesntHave() {
+    if (!this.skills) {
+      return [];
+    }
+    if (!this.addedSkills) {
+      this.addedSkills = [];
+    }
+    const skillsReturn = [];
+    this.skills.forEach(skill => {
+      const doesExist = this.addedSkills.findIndex(
+        o1 =>  o1.id === skill.id
+      );
+      if (doesExist) {
+        skillsReturn.push(skill);
+      }
+    });
+    return skillsReturn;
+  }
 }
