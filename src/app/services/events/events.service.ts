16db4ee2c182a28944865417ad653a9cc7cb3c41
Orrin <naylor.orrin@gmail.com>
2018-09-08 20:01:18 -0600

    fixed merge issue

--- src/app/services/events/events.service.ts
+++ src/app/services/events/events.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs/Observable';
+import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../../environments/environment';
 
@@ -43,28 +43,29 @@ export class EventsService {
 
   createEvent(name: String): Observable<Event[]> {
     const jwt = localStorage.getItem('jwt-token');
-    return fetch(environment.host + '/api/events/createevent',
-    {
-      method: 'POST', // *GET, POST, PUT, DELETE, etc.
-        mode: 'no-cors', // no-cors, cors, *same-origin
-        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-        credentials: 'same-origin', // include, same-origin, *omit
-        headers: {
-            'Content-Type': 'application/json; charset=utf-8',
-            // 'Content-Type': 'application/x-www-form-urlencoded',
-        },
-        redirect: 'follow', // manual, *follow, error
-        referrer: 'no-referrer', // no-referrer, *client
-        body: JSON.stringify({
-          name,
-          jwt,
-        }),
-    })
-    .then(function(response) {
-      return response.json();
-    })
-    .then(function(events) {
-      return events;
-    });
+    return this.http.get<Event[]>(environment.host + '/api/events/getevents');
+    // return fetch(environment.host + '/api/events/createevent',
+    // {
+    //   method: 'POST', // *GET, POST, PUT, DELETE, etc.
+    //     mode: 'no-cors', // no-cors, cors, *same-origin
+    //     cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+    //     credentials: 'same-origin', // include, same-origin, *omit
+    //     headers: {
+    //         'Content-Type': 'application/json; charset=utf-8',
+    //         // 'Content-Type': 'application/x-www-form-urlencoded',
+    //     },
+    //     redirect: 'follow', // manual, *follow, error
+    //     referrer: 'no-referrer', // no-referrer, *client
+    //     body: JSON.stringify({
+    //       name,
+    //       jwt,
+    //     }),
+    // })
+    // .then(function(response) {
+    //   return response.json();
+    // })
+    // .then(function(events) {
+    //   return events;
+    // });
   }
 }
